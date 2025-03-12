7ac0b4d7fe6c1f8e03adeb124e707db271538751
Aaron David <aarond2005@icloud.com>
2025-03-12 22:33:05 +0000

    Implemented basic base html, urls, views and settings changes

--- Quizzical/app/views.py
+++ Quizzical/app/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    ## Using empty context for now and base.html just to set up
+    ## the views and urls
+    context = {'boldmessage': 'home'}
+    return render(request, 'app/base.html', context)
+
+def login(request):
+    context = {'boldmessage': 'login'}
+    return render(request, 'app/base.html', context)
+
+def signup(request):
+    context = {'boldmessage': 'signup'}
+    return render(request, 'app/base.html', context)
+
+def account(request):
+    ## Gonna leave the account urls up to whoever does it
+    context = {'boldmessage': 'account'}
+    return render(request, 'app/base.html', context)
+
+def create_quiz(request):
+    context = {'boldmessage': 'create_quiz'}
+    return render(request, 'app/base.html', context)
+
+def category(request):
+    context = {'boldmessage': 'category'}
+    return render(request, 'app/base.html', context)
+
+def quiz(request):
+    context = {'boldmessage': 'quiz'}
+    return render(request, 'app/base.html', context)
