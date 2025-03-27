cc57e7891d42af8703c603174e2ad06e1584e646
Aaron David <aarond2005@icloud.com>
2025-03-27 14:52:56 +0000

    Implemented logging in and out of accounts

--- Quizzical/app/urls.py
+++ Quizzical/app/urls.py
@@ -10,10 +10,11 @@ from app import views
 app_name = 'app'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+    path('login/', views.user_login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('account/', views.account, name='account'),
     path('create_quiz/', views.create_quiz, name='create_quiz'),
     path('category/', views.category, name='category'),
     path('quiz/', views.quiz, name='quiz'),
+    path('logout/', views.user_logout, name='logout'),
 ]
